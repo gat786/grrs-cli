791d1a6d6952b56cb2ce4f18927a0c8b96238237
Ganesh Tiwari <ganesht049@gmail.com>
2026-06-28 18:24:53 +0530

    ? mark

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -6,7 +6,7 @@ struct CLI {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = CLI::parse();
 
     // maybe try implementing using BufReader so that all the file
@@ -16,18 +16,22 @@ fn main() {
     // you can just do expect and handle it concisely without taking much space,
     // it will still panic but with less effort as a programmer required from you
     let result = std::fs::read_to_string(&args.path); // .expect("could not read file");
+    // ? - can also be used to eliminate the error return type, when used
+    // if an error is received `?` will automatically return the error value, eliminating
+    // the need to handle it in an err block
 
-    match result {
-        Ok(content) => {
-            for line in content.lines() {
-                if line.contains(&args.pattern) {
-                    println!("{}", line);
-                }
-            }
-            println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
-        }
+    let content = match result {
+        Ok(content) => content,
         Err(error) => {
-            println!("Oh noes!: {}", error);
+            return Err(error.into());
+        }
+    };
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
         }
     }
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    return Ok(());
 }
