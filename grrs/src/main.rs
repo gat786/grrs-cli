e65f61423a5c074cfb48f42458a84b1beffb9691
Ganesh Tiwari <ganesht049@gmail.com>
2026-06-28 18:01:27 +0530

    comit msg

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -11,6 +11,10 @@ fn main() {
 
     // maybe try implementing using BufReader so that all the file
     // does not get loaded into memory at once
+    // expect lets you handle results, which are enums, when you dont want to deal with
+    // resultant enums because you consider the failure to be most likely not happening
+    // you can just do expect and handle it concisely without taking much space,
+    // it will still panic but with less effort as a programmer required from you
     let result = std::fs::read_to_string(&args.path); // .expect("could not read file");
 
     match result {
